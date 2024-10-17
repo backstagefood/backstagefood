855c5b8ca8fa9128588133c14321eaca384e9c55
Daniel Moreira Cardoso <dmcardoso.99@hotmail.com>
2024-10-17 20:12:29 -0300

    refactor: change files of dto

--- internal/handlers/customer.go
+++ internal/handlers/customer.go
@@ -1,10 +1,13 @@
 package handlers
 
 import (
+	"github.com/backstagefood/backstagefood/internal/domain"
 	"github.com/backstagefood/backstagefood/internal/repositories"
 	"github.com/backstagefood/backstagefood/internal/service"
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 	"net/http"
+	"time"
 )
 
 type CustomerHandler struct {
@@ -19,18 +22,42 @@ func NewCustomerHandler(databaseConnection *repositories.ApplicationDatabase) *C
 	}
 }
 
+type SignUpCustomerDTO struct {
+	Name  string `json:"name"`
+	CPF   string `json:"cpf"`
+	Email string `json:"email"`
+}
+
+type CustomerDTO struct {
+	ID    string `json:"id"`
+	Name  string `json:"name"`
+	CPF   string `json:"cpf"`
+	Email string `json:"email"`
+}
+
+func (customerDTO *SignUpCustomerDTO) ToDomainCustomer() *domain.Customer {
+	return &domain.Customer{
+		ID:        uuid.New().String(),
+		Name:      customerDTO.Name,
+		CPF:       customerDTO.CPF,
+		Email:     customerDTO.Email,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+}
+
 // CustomerSignUp godoc
 // @Summary Customer sign up
 // @Description Create a customer.
 // @Tags customers
 // @Produce json
-// @Param customer body service.SignUpCustomerDTO true "SignUpCustomerDTO object"
-// @Success 201 {object} service.CustomerDTO
+// @Param customer body handlers.SignUpCustomerDTO true "SignUpCustomerDTO object"
+// @Success 201 {object} handlers.CustomerDTO
 // @Failure 400 {object} map[string]string
 // @Failure 500 {object} map[string]string
 // @Router /customers/sign-up [post]
 func (h *CustomerHandler) CustomerSignUp(c echo.Context) error {
-	productDTO := new(service.SignUpCustomerDTO)
+	productDTO := new(SignUpCustomerDTO)
 	if err := c.Bind(productDTO); err != nil {
 		return c.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
 	}
@@ -40,7 +67,7 @@ func (h *CustomerHandler) CustomerSignUp(c echo.Context) error {
 		return c.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
 	}
 
-	return c.JSON(http.StatusCreated, service.CustomerDTO{
+	return c.JSON(http.StatusCreated, CustomerDTO{
 		ID:    customer.ID,
 		Name:  customer.Name,
 		CPF:   customer.CPF,
@@ -54,7 +81,7 @@ func (h *CustomerHandler) CustomerSignUp(c echo.Context) error {
 // @Tags customers
 // @Produce json
 // @Param cpf path string true "Customer CPF"
-// @Success 200 {object} service.CustomerDTO
+// @Success 200 {object} handlers.CustomerDTO
 // @Failure 500 {object} map[string]string
 // @Router /customers/{cpf} [get]
 func (h *CustomerHandler) CustomerIdentify(c echo.Context) error {
@@ -65,7 +92,7 @@ func (h *CustomerHandler) CustomerIdentify(c echo.Context) error {
 		return c.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
 	}
 
-	return c.JSON(http.StatusCreated, service.CustomerDTO{
+	return c.JSON(http.StatusCreated, CustomerDTO{
 		ID:    customer.ID,
 		Name:  customer.Name,
 		CPF:   customer.CPF,
