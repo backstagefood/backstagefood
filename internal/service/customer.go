cfd074a5072a1b1eb390c29f81ff801a8bf6d54b
Daniel Moreira Cardoso <dmcardoso.99@hotmail.com>
2024-10-17 20:12:29 -0300

    refactor: change files of dto

--- internal/service/customer.go
+++ internal/service/customer.go
@@ -3,36 +3,10 @@ package service
 import (
 	"github.com/backstagefood/backstagefood/internal/domain"
 	"github.com/backstagefood/backstagefood/internal/repositories"
-	"github.com/google/uuid"
-	"time"
 )
 
 type CustomerService struct {
 	customerRepository repositories.CustomerRepository
-}
-
-type SignUpCustomerDTO struct {
-	Name  string `json:"name"`
-	CPF   string `json:"cpf"`
-	Email string `json:"email"`
-}
-
-type CustomerDTO struct {
-	ID    string `json:"id"`
-	Name  string `json:"name"`
-	CPF   string `json:"cpf"`
-	Email string `json:"email"`
-}
-
-func (customerDTO *SignUpCustomerDTO) ToDomainCustomer() *domain.Customer {
-	return &domain.Customer{
-		ID:        uuid.New().String(),
-		Name:      customerDTO.Name,
-		CPF:       customerDTO.CPF,
-		Email:     customerDTO.Email,
-		CreatedAt: time.Now(),
-		UpdatedAt: time.Now(),
-	}
 }
 
 func NewCustomerService(repository repositories.CustomerRepository) *CustomerService {
