cf7e27d146fc225b8589c931030de65998289ca5
Andre Leite <andrleite@gmail.com>
2024-10-20 11:30:54 -0300

    CRUD for products domain

--- internal/repositories/products.go
+++ internal/repositories/products.go
@@ -2,6 +2,8 @@ package repositories
 
 import (
 	"database/sql"
+	"fmt"
+
 	"github.com/backstagefood/backstagefood/internal/core/domain"
 	portRepository "github.com/backstagefood/backstagefood/internal/core/ports/repositories"
 )
@@ -107,4 +109,69 @@ func (s *productRepository) GetCategoryID(categoryDescription string) (string, e
 		return "", err
 	}
 	return categoryID, nil
+}
+
+func (s *productRepository) UpdateProduct(product *domain.Product) (*domain.Product, error) {
+	// Validade UUID fields
+	if product.ID == "" {
+		return nil, fmt.Errorf("invalid product ID")
+	}
+	if product.IDCategory == "" {
+		return nil, fmt.Errorf("invalid product category ID")
+	}
+	query := "UPDATE products SET id_category = $1, description = $2, ingredients = $3, price = $4, updated_at = $5 WHERE id = $6"
+	stmt, err := s.sqlClient.Prepare(query)
+	defer stmt.Close()
+	if err != nil {
+		return nil, err
+	}
+	_, err = stmt.Exec(
+		product.IDCategory,
+		product.Description,
+		product.Ingredients,
+		product.Price,
+		product.UpdatedAt,
+		product.ID)
+	if err != nil {
+		return nil, err
+	}
+	return product, nil
+}
+
+func (s *productRepository) DeleteProduct(productID string) error {
+	query := "DELETE FROM products WHERE id = $1"
+	stmt, err := s.sqlClient.Prepare(query)
+	defer stmt.Close()
+	if err != nil {
+		return err
+	}
+	_, err = stmt.Exec(productID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// GetCategories returns a list of all product categories
+func (s *productRepository) GetCategories() ([]*domain.ProductCategory, error) {
+	query := "SELECT id, description FROM product_categories"
+	stmt, err := s.sqlClient.Prepare(query)
+	defer stmt.Close()
+	if err != nil {
+		return nil, err
+	}
+	rows, err := stmt.Query()
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	categories := make([]*domain.ProductCategory, 0)
+	for rows.Next() {
+		var category domain.ProductCategory
+		if err := rows.Scan(&category.ID, &category.Description); err != nil {
+			return nil, err
+		}
+		categories = append(categories, &category)
+	}
+	return categories, nil
 }
