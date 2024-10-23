d9b17e540a6e9735f7b2304f476cf49a5b2bc981
Hammilton Geraldo Fantin <hfantin@gmail.com>
2024-10-22 21:08:47 -0300

    feat: orders crud

--- internal/core/ports/repositories/order.go
+++ internal/core/ports/repositories/order.go
@@ -4,4 +4,6 @@ import "github.com/backstagefood/backstagefood/internal/core/domain"
 
 type Order interface {
 	UpdateOrderStatus(orderId string) (*domain.Order, error)
+	ListOrders() ([]*domain.Order, error)
+	CreateOrder(product *domain.Order) (*domain.Order, error)
 }
--- internal/handlers/order.go
+++ internal/handlers/order.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"fmt"
+	"github.com/backstagefood/backstagefood/internal/core/domain"
 	portService "github.com/backstagefood/backstagefood/internal/core/ports/services"
 	"github.com/backstagefood/backstagefood/internal/repositories"
 	"net/http"
@@ -33,16 +35,59 @@ func NewOrderHandler(databaseConnection *repositories.ApplicationDatabase) *Orde
 // @Failure 400 {object} map[string]string
 // @Failure 500 {object} map[string]string
 // @Router /checkout/{orderId} [post]
-func (h *OrderHandler) Checkout(c echo.Context) error {
+func (o *OrderHandler) Checkout(c echo.Context) error {
 	orderId := c.Param("orderId")
 	if orderId == "" {
 		return c.JSON(http.StatusBadRequest, map[string]string{"error": "order id maybe not exist"})
 	}
 
-	result, err := h.orderService.MakeCheckout(orderId)
+	result, err := o.orderService.MakeCheckout(orderId)
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, map[string]string{"error": err.Error()})
 	}
 
 	return c.JSON(http.StatusCreated, result)
+}
+
+// ListAllOrders godoc
+// @Summary List all orders
+// @Description Get all orders available in the database.
+// @Tags orders
+// @Produce json
+// @Success 200 {array} domain.Order
+// @Failure 500 {object} map[string]string
+// @Router /orders [get]
+func (o *OrderHandler) ListAllOrders(c echo.Context) error {
+	result, err := o.orderService.GetOrders()
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": err.Error()})
+	}
+	return c.JSON(http.StatusOK, result)
+}
+
+// CreateOrder godoc
+// @Summary Create a new order
+// @Description Create a new order in the database.
+// @Tags orders
+// @Accept json
+// @Produce json
+// @Param order body services.CreateOrderDTO true "CreateOrderDTO object"
+// @Success 201 {object} services.CreateOrderDTO
+// @Failure 400 {object} map[string]string
+// @Failure 500 {object} map[string]string
+// @Router /orders [post]
+func (o *OrderHandler) CreateOrder(c echo.Context) error {
+	createOrderDTO := new(portService.CreateOrderDTO)
+	fmt.Println("orderBody=", createOrderDTO)
+	if err := c.Bind(createOrderDTO); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
+	}
+	newOrder := domain.Order{CustomerID: createOrderDTO.CustomerID}
+	order, err := o.orderService.CreateOrder(&newOrder)
+	fmt.Println("order ", order)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
+	}
+	return c.JSON(http.StatusCreated, order)
+
 }
